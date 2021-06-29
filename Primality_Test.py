b371d98e4775c973c6d83fb4505ca412a085d827
SofALof <57773661+SofALof@users.noreply.github.com>
2021-06-29 22:09:54 +0200

    Update Primality_Test.py
    updated to return true after while loop (simple error correction)

--- Primality_Test.py
+++ Primality_Test.py
@@ -4,25 +4,27 @@ Primality Test: 6k+-1 Optimization
 
 Created: June 29 2021
 '''
+import math
 
 def primalityTest(n):
 
     if n <= 3: #checks small numbers (easily done as only 2 and 3 are Prime)
         return n>1
 
-    if n mod 2 == 0 or n mod 3 == 0: #optimization to check if divisible by 2 or 3
+    if n % 2 == 0 or n % 3 == 0: #optimization to check if divisible by 2 or 3
         return False
 
     i = 5 #helper integer taking the form 6k-1
 
-    radN = sqrt(n) #last val that needs to b cross-refrenced
+    radN = math.sqrt(n) #last val that needs to b cross-refrenced
     while i <= radN: 
 
-        if n mod i == 0 or n mod (i+2) == 0: #check n against all forms of 6k+-1 
+        if n % i == 0 or n % (i+2) == 0: #check n against all forms of 6k+-1 
             return False                                # Note that i+2 = 6k+1
 
         i += 6 #move i to the next iteration of 6k-1
 
+    return True
 
 
 ### Explanation of Algorithm ###
@@ -50,7 +52,7 @@ less than sqrt(N) --> (as all other numbers will contain the above primes)
 
 i = 5 <-- the first instance of 6k-1
 
-while  i**2 <= n <-- breaks if all possible primes have been checked or if i > rad(n)
+while  i <= rad(n) <-- breaks if all possible primes have been checked or if i > rad(n)
 
 if n mod i == 0 or n mod i+2 == 0 <-- checks if n is divisible by potential primes 6k+1
                                                         (i takes the form 6k-1 and i+2 takes the form 6k+1)
@@ -62,3 +64,5 @@ i += 6 <-- brings i to the next iteration of 6k-1
 
 '''
 
+print(primalityTest(2))
+print(primalityTest(19))
