1558f891e4ce0d6fdaca94895fa897cd318e61a8
SofALof <57773661+SofALof@users.noreply.github.com>
2021-06-29 20:54:04 +0200

    Create Primality_Test.py
    Primality Test using the 6k+1 Number Theory Optimization

---
+++ Primality_Test.py
@@ -0,0 +1,63 @@
+'''
+Sofia Medina - Algorithmic Investigation Summer 2021
+Primality Test: 6k+-1 Optimization
+
+Created: June 29 2021
+'''
+
+def primalityTest(n):
+
+    if n <= 3: #checks small numbers (easily done as only 2 and 3 are Prime)
+        return n>1
+
+    if n mod 2 == 0 or n mod 3 == 0: #optimization to check if divisible by 2 or 3
+        return False
+
+    i = 5 #helper integer taking the form 6k-1
+
+    while i**2 <= n: #same thing as i <= rad(n); just a cleaner operation
+
+        if n mod i == 0 or n mod (i+2) == 0: #check n against all forms of 6k+-1 
+            return False                                # Note that i+2 = 6k+1
+
+        i += 6 #move i to the next iteration of 6k-1
+
+
+
+### Explanation of Algorithm ###
+'''
+
+##Three Initial Optimizations ##
+    n <= 3
+        - serves to weed out negative numbers
+        - 0, 1, 2, and 3 as they are kinda "funky" and do not always hold to the
+        general rules
+        - The above numbers are also small enough where it is easy to take this liberty
+
+  if  n mod 2 == 0
+        - checks if the number is even
+        - and if it is even that it is pretty obviously not prime
+
+    if n mod 3 == 0
+        - checks if the number is divisible by 3 and so such not prime
+
+##The While Loop##
+- Prime numbers take the form 6k+-1
+- When checking if a number N is prime it is only necceasry to check the PRIME numbers
+less than sqrt(N) --> (as all other numbers will contain the above primes)
+
+
+i = 5 <-- the first instance of 6k-1
+
+while  i**2 <= n <-- breaks if all possible primes have been checked or if i > rad(n)
+
+if n mod i == 0 or n mod i+2 == 0 <-- checks if n is divisible by potential primes 6k+1
+                                                        (i takes the form 6k-1 and i+2 takes the form 6k+1)
+
+i += 6 <-- brings i to the next iteration of 6k-1
+
+
+
+
+'''
+
