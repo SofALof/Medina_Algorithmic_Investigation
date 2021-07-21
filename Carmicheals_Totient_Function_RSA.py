14690c666be3a1a20c5c05b745ea42ebf11b7c6a
SofALof <57773661+SofALof@users.noreply.github.com>
2021-07-20 19:32:39 -0500

    Update Carmicheals_Totient_Function_RSA.py
    Ok updated the explanation for carmicheal...have yet to discuss its connection w/ the encryption/decryption process (will prob use the expo in the euclid totient function file as inspiration) furthermore I dont really know what the carmicheal functions aims to find...i understand that it is an optimization and that its based of off phi(n) but thats only rlly for RSA...i would like i one line explanation of carmicheals general purpose

--- Carmicheals_Totient_Function_RSA.py
+++ Carmicheals_Totient_Function_RSA.py
@@ -14,7 +14,30 @@ def CarmichealsTotient(p,q):
     return  (abs( (p-1)*(q-1) ) )/ (eucldAlg(p-1,q-1) )
 
 
-print(CarmichealsTotient(11,5))
+
+### Explanation of Algorithm ###
+'''
+Carmicheal's totient function; sometimes called lambda(n)
+
+- The exact thing it does...I couldnt tell you right now
+
+- (For RSA), serves as an optimization to Euler's totient function as it essentially
+"reduces" the totient (while maintaining its modular properties)
+making it a smaller number and thus making operations quicker
+
+- Sometimes also referred to as the "reduced totient function" or the least universal
+exponent function
+
+
+lambda(n) = lambda(pq) = lcm (phi(p), phi(q) ) = lcm(p-1, q-1)
+'''
+
+
+
+
+
+
+#print(CarmichealsTotient(11,5))
 
 
 
